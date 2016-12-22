5d370162bc7980ef0966bd40986ad7cd8fc68da4
edward-ly <edvard.ly@gmail.com>
2016-12-22 11:50:06 -0800

    added comments for planning implementation

--- palindrome/tester.rb
+++ palindrome/tester.rb
@@ -2,12 +2,38 @@ class Tester
   class T1
     def palindrome?(string)
       # first implementation
+      # treat string as an array of characters
+
+      # 1. check edge cases first
+
+
+
+      # 2. remove non-alphabet characters from string
+
+
+
+      # 3. convert all letters to lowercase for comparison
+
+
+
+      # 4. check eack pair of characters one at a time for matches
+
+
     end
   end
 
   class T2
     def palindrome?(string)
       # second implementation
+      # use "reverse" method to compare strings
+
+      # 1-3. process string in the same manner as before
+
+
+
+      # 4. compare string with reverse string and return result
+
+
     end
   end
 end
